3826bb82d3a0c0f360aabd275956aa740c6d14fa
aayush031 <aayushroy813@gmail.com>
2026-03-28 01:02:56 +0530

    added base url in api js of render to connect backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:  "https://auto-email-scheduler-9zx6.onrender.com/api"
+  baseURL:  "https://auto-email-scheduler-3.onrender.com"
 });
 
 export default api;
